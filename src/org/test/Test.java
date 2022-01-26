853aaffc4bc8a41963bd35950fc1f5ae47d967c9
UdayMadhu <udayb.techit@gmail.com>
2022-01-26 17:49:40 +0530

    Project 2

---
+++ src/org/test/Test.java
@@ -0,0 +1,30 @@
+package org.test;
+
+public class Test {
+	public static void main(String[] args) {
+		String s = null;
+
+//		int parseInt = Integer.parseInt(s);
+//		System.out.println(parseInt);
+		
+		
+		double parseDouble = Double.parseDouble(s);
+		System.out.println(parseDouble);
+		
+		float parseFloat = Float.parseFloat(s);
+		System.out.println(parseFloat);
+		
+		
+		
+		/*try {
+			int parseInt = Integer.parseInt(s);
+			System.out.println(parseInt);
+			
+		} catch (Exception e) {
+
+				
+		}*/
+	}
+	
+
+}
